762b8a94f54b0c8ef844d7a67adde735c4c9c51c
Justin Kemp <jkemp0790@gmail.com>
2023-01-12 17:38:33 -0600

    added components for champs, members, rules, etc, created routes for multi-page app

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Nav } from "./Nav";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./Home";
+export { default as Rules } from "./Rules";
+export { default as Members } from "./Members";
+export { default as Champions } from "./Champions";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Nav,
+  Footer,
+  Home,
+  Rules,
+  Members,
+  Champions,
+} from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rulesandregulations" element={<Rules />} />
+      <Route path="/currentmembers" element={<Members />} />
+      <Route path="/pastchampions" element={<Champions />}>
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
