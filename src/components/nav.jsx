7b35dadc74b57d306f5dc30dd69a786d6cf8aa33
Justin Kemp <jkemp0790@gmail.com>
2023-01-21 00:11:07 -0600

    mobile navbar updates, mobile styling and nav placement

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,56 +1,68 @@
 import React from "react";
+import { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
-import { Bars3Icon } from '@heroicons/react/24/solid';
 import "./Nav.css";
 
 
-function Nav() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
+class Nav extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
 
-  return (
-    <div className="navigator">
-      <nav className="navbar navbar-expand navbar-dark">
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            <img src="../full-logo.png" alt="full-logo" height={220} />
-          </NavLink>
-          <div className="hamburger">
-            <Bars3Icon className="hamburger h-6 w-6 text-blue-500" onClick={() => setIsNavExpanded(isNavExpanded => !isNavExpanded)}/>
-          </div>
-          <div
-            className={
-              isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-            }
-          >
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/rulesandregulations">
-                  Rules & Regulations
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/currentmembers">
-                  Members
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/pastchampions">
-                  Champions
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+  render() {
+    //CLOSE NAV MENU ON SCROLL
+    window.onscroll = () => {
+      document.getElementById('navbar').classList.remove('active');
+      // document.getElementById('bar').classList.toggle('clicked');
+    };
+
+    //CLOSE NAV MENU ON CLICK
+    window.onclick = (event) => {
+      if (event.target !== document.getElementById('navbar') && event.target !== document.getElementById('bar')) {
+        setTimeout(() => {
+          document.getElementById('navbar').classList.remove('active');
+          // document.getElementById('bar').classList.toggle('clicked');
+        }, 300);
+      }
+    }
+
+    return (
+      <nav>
+        <NavLink className="navbar-brand" to="/">
+          <img src="../full-logo.png" alt="full-logo" height={200} />
+        </NavLink>
+        <div>
+          <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+            <li>
+              <NavLink className="active" to="/">
+                Home
+                <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/rulesandregulations">
+                Rules
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/currentmembers">
+                Members
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/pastchampions">
+                Champions
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div id="mobile" onClick={this.handleClick}>
+          <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
       </nav>
-    </div>
-  );
+    );
+  }
 }
 
 export default Nav;
